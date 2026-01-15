d4114b676e83109dc3e7e52ec3ec18bc32fdace5
isaiasfl <ifernandez@ieshlanz.es>
2026-01-15 02:13:45 +0100

    Ejemplo-02

---
+++ Ejemplo-02/luis.js
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio de Luis Pérez
+ * ID: JS-001
+ */
+
+function suma(a, b) {
+    // Uso var para ver si el profesor (IA) me recomienda usar let o const
+    var resultado = a + b;
+    return resultado;
+}
+
+// Llamada de prueba
+console.log("El resultado de la suma es: " + suma(5, 10));
+
+/* Nota para el profesor:
+ * He intentado que sea lo más simple posible.
+ */
